e529a3a8a4da71d15119bc4a9464fb39f34d0203
youtora <youtora63@gmail.com>
2026-04-22 01:12:22 +0300

    ג
    ר'ק

--- functions/sitemap.xml.js
+++
@@ -1,6 +0,0 @@
-import { buildSitemapIndex, xmlResponse } from "./_sitemap.js";
-
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  return xmlResponse(buildSitemapIndex(url.origin));
-}
